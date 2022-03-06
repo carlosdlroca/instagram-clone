a8be9afa47abf73f38b423aba8bec27717883eb9
unknown <carlosdlroca80@gmail.com>
2022-03-06 15:30:23 -0800

    require mysql and dbPool in index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const mysql = require('mysql');
+const pool = require("./dbPool");
+
 const PORT = process.env.PORT || 8081;
 
 
