3cc56df9e4cfc3c2f8dda1eb4cc45f2ca8af8fdd
unknown <carlosdlroca80@gmail.com>
2022-03-07 14:23:55 -0800

    use routes

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,21 @@ const app = express();
 
 const mysql = require('mysql');
 const pool = require("./dbPool");
+const authenticateToken = require("./middleware/auth");
+
+const authRoutes = require("./routes/auth");
+const postsRoutes = require("./routes/posts");
+
 
 const PORT = process.env.PORT || 8081;
+
+app.use(express.json())
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postsRoutes);
+
+app.get("/", authenticateToken, (req, res) => {
+    res.send(req.user);
+})
 
 
 
