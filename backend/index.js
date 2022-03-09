ae8c4fb7baec14ed566f0e4b32384a1a1640f284
unknown <carlosdlroca80@gmail.com>
2022-03-08 20:59:50 -0800

    remove mysql require statements

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const mysql = require('mysql');
-const pool = require("./dbPool");
 const authenticateToken = require("./middleware/auth");
 
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 const postsRoutes = require("./routes/posts");
 
 
@@ -15,6 +14,7 @@ const PORT = process.env.PORT || 8081;
 
 app.use(express.json())
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 app.use("/api/posts", postsRoutes);
 
 app.get("/", authenticateToken, (req, res) => {
