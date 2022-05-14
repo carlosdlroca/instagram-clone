d165df7a0828a2ae4bd71eb81dbf4089f3514d9d
unknown <carlosdlroca80@gmail.com>
2022-05-14 13:56:15 -0700

    generate css variables for colors from the constant colors object

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,26 @@
+import {useEffect} from "react";
+import Head from "next/head"
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
+import { colors } from "../constants/styles";
+
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+
+    // For every color in the CONSTANTS file, set the global css variable.
+    Object.entries(colors).forEach(([property, value]) => {
+      document.documentElement.style.setProperty(`--${property}`, value);
+    });
+
+  }, [])
+
   return (
     <Layout>
+      <Head>
+        <meta name="theme-color" content={colors['color-primary']} />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   ); 
